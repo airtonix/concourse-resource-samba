d5c428b94669808236736dbacc6ea6f499778047
Zenobius Jiricek <airtonix@gmail.com>
2016-12-24 08:26:26 +1030

    Fix: reset base to bash

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,7 @@
-FROM mhart/alpine-node:7
+FROM alpine:latest
+
 MAINTAINER Zeno Jiricek <airtonix@gmail.com>
 
 RUN mkdir /mnt/storage
-ADD ./resource/in /opt/resource/in
-ADD ./resource/out /opt/resource/out
-ADD ./resource/check /opt/resource/check
-ADD ./resource/package.json /opt/resource/package.json
-ADD ./resource/lib/* /opt/resource/lib
+ADD ./resource /opt/resource
 RUN chmod +x /opt/resource/*
--- test/run.sh
+++ test/run.sh
@@ -9,15 +9,7 @@ export BUILD_JOB_NAME='my-build-job'
 export BUILD_PIPELINE_NAME='my-build-pipeline'
 export ATC_EXTERNAL_URL='127.0.0.1/atc'
 
-chown root:root /opt/resource/* -R
-chmod +x /opt/resource/* -R
-if [ ! -d ./node_modules ]; then
-	echo "${LOG_PREFIX}.npm.install"
-	npm install
-fi
 
-EXPORT=resource-samba*
-
-cat $1 | node /opt/resource/check
+cat $1 | /opt/resource/check
 
 echo "${LOG_PREFIX}.end"
